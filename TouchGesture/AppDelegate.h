9ba561d2aa7eba99857aa990ca6e86cd6b2cfdd6
MadhuriJagtap <madhuri@felix-its.com>
2016-08-10 15:55:15 +0530

    Initial Commit

---
+++ TouchGesture/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TouchGesture
+//
+//  Created by Felix-Madhuri on 10/08/16.
+//  Copyright © 2016 Felix-Madhuri. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TouchGesture/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TouchGesture
+//
+//  Created by Felix-Madhuri on 10/08/16.
+//  Copyright © 2016 Felix-Madhuri. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
